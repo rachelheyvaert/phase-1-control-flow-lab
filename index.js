e9d2b089b44fb9e243d54390df28af71636ea826
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-01-10 19:56:05 -0500

    stopping point

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(depth){
   // Write your code here!
+  if(depth <= 400)
+    return 'This one is on me!'
+} if (depth > 2000) { //Why not passing??
+  scuberGreetingForFeet =  'I will gladly take your thirty bucks.';
+} else if (depth > 2500) { //Why not passing??
+  scuberGreetingForFeet = 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
+ternaryCheckCity('NYC');
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      case 'not as generous':
+        return 'Thank you.'
+        default :
+        return 'Bye.'
+}
 }
