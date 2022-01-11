06bf7dd5734500ce52a99c0a9f9eccb6dd167ee0
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-01-11 09:21:48 -0500

    formating

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-function scuberGreetingForFeet(depth){
+function scuberGreetingForFeet(depth) {
   // Write your code here!
-  if(depth <= 400) {
+  if (depth <= 400) 
     return 'This one is on me!'
-  }
-} if(depth >= 2001){ //Why not passing??
-   return 'I will gladly take your thirty bucks.'
+} if (depth >= 2001) { //Why not passing??
+  return 'I will gladly take your thirty bucks.'
 } else if (depth > 2501) { //Why not passing??
   return 'No can do.'
 }
+}
 
-
-function ternaryCheckCity(city){
+function ternaryCheckCity(city) {
   // Write your code here!
   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 ternaryCheckCity('NYC');
 
-function switchOnCharmFromTip(tip){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
-  switch(tip) {
+  switch (tip) {
     case 'generous':
       return 'Thank you so much.'
-      case 'not as generous':
-        return 'Thank you.'
-        default :
-        return 'Bye.'
-}
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
